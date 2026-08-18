745162e1ac096b58d53eecd382342c8fa096b7b7
shauryajain <shaurya9@uw.edu>
2026-08-18 14:44:37 +0530

    Adding a new tool: csv-reader

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from tools.registry import ToolRegistry, Tool
 from controller import Controller
 from tools.calculator import Calculator
 from tools.read_file import ReadFile
+from tools.read_csv import ReadCSV
 
 
 memory = Memory()
@@ -45,8 +46,26 @@ read_file_tool = Tool(name="file_reader",
                                                         "the directory")}},
                                  "required": ["file_path"]})
 
+read_csv = ReadCSV()
+read_csv_tool = Tool(name="csv_reader",
+                     description="Reads a csv file that has been entered and"
+                     "returns the contents, including the column names and"
+                     "titles",
+                     function=read_csv.execute,
+                     arguments={"type": "object",
+                                "properties": {
+                                    "file_path": {
+                                        "type": "string",
+                                        "description": ("The path of the csv"
+                                                        "file to read,"
+                                                        "relative to the"
+                                                        "directory")}},
+                                "required": ["file_path"]})
+
 tools.register(calculator_tool)
 tools.register(read_file_tool)
+tools.register(read_csv_tool)
+
 agent = Agent(llm=llm_client, tools=tools, memory=memory,
               prompt_builder=prompt_builder)
 
